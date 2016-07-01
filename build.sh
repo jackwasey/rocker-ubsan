7cb77163253b73500cfae63583108a70d30f5543
Jack Wasey <jack@jackwasey.com>
2016-07-01 09:43:41 -0400

    script that updates and builds all environments

--- build.sh
+++ build.sh
@@ -9,5 +9,14 @@ if [[ -z "$dir" ]]; then
     exit 1
 fi
 
+# for simplicity, always check LLVM release version:
 
-docker build -t "jackwasey/$dir" "$dir"
+LLVM_REL_VER=$(curl -s 'https://llvm.org/svn/llvm-project/llvm/tags/' | sed 's/<[^>]*>//g' | grep RELEASE | sed 's/^[[:space:]]*RELEASE_//g' | sed 's/\/$//' | tail -1)
+
+LLVM_REL_ARG=""
+if [ "$dir" = "clang-3.8" ]; then
+    LLVM_REL_ARG="--build-arg=LLVM_REL_VER=${LLVM_REL_VER}"
+    printf "using LLVM_REL_ARG = %s\n" "$LLVM_REL_ARG"
+fi
+
+docker build $LLVM_REL_ARG -t "jackwasey/$dir" "$dir"
--- update-all.sh
+++ update-all.sh
@@ -5,10 +5,13 @@ IFS=$'\n\t'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-docker pull r-devel
+docker pull rocker/r-devel
 docker pull debian:stretch
 
 for dir in */ ; do
-    printf "building %s%s%s" "$RED" "$dir" "$NC"
-    ./build.sh "$dir"
+    #dir_trimmed=${dir%/}
+    dir_trimmed=$(echo "$dir" | sed 's:/*$::')
+    #printf "building %s%s%s" "$RED" "$dir_trimmed" "$NC"
+    printf "building %s\n" "$dir_trimmed"
+    ./build.sh "$dir_trimmed"
 done
