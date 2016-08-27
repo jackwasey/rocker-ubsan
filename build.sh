afc671bbbf5ec826a034d2a1a8f865a7b669c49e
Jack Wasey <jack@jackwasey.com>
2016-08-27 14:48:28 -0400

    work with various llvm version qualifiers, etc

--- build.sh
+++ build.sh
@@ -9,26 +9,44 @@ if [[ -z "$JW_DOCKER_BUILD_DIR" ]]; then
     exit 1
 fi
 
-# for simplicity, always check LLVM release version, even if not compiling LLVM
-LLVM_REL_VER=
-
+LLVM_REL_STR=""
+LLVM_REL_VER=""
 LLVM_REL_VER_ARG=""
+LLVM_REL_STATUS=""
 LLVM_REL_STATUS_ARG=""
 case "$JW_DOCKER_BUILD_DIR" in
-    clang-latest)
-    LLVM_REL_VER=$(curl -s 'https://llvm.org/svn/llvm-project/llvm/tags/' | sed 's/<[^>]*>//g' | grep RELEASE | sed 's/^[[:space:]]*RELEASE_//g' | sed 's/\/$//' | tail -1)
+    clang-trunk)
+    LLVM_REL_STR=RELEASE
+    ;;
+    clang-3.9)
+    LLVM_REL_STR=RELEASE_39
     ;;
     clang-3.8)
-    LLVM_REL_VER=$(curl -s 'https://llvm.org/svn/llvm-project/llvm/tags/' | sed 's/<[^>]*>//g' | grep RELEASE_38 | sed 's/^[[:space:]]*RELEASE_//g' | sed 's/\/$//' | tail -1)
+    LLVM_REL_STR=RELEASE_38
+    ;;
+    clang-3.7)
+    LLVM_REL_STR=RELEASE_37
     ;;
     *)
     echo "unknown clang version requested via JW_DOCKER_BUILD_DIR"
-    exit 1
 esac 
-LLVM_REL_VER_ARG="--build-arg=LLVM_REL_VER=${LLVM_REL_VER}"
-printf "using LLVM_REL_VER_ARG = %s\n" "$LLVM_REL_VER_ARG"
-LLVM_REL_STATUS=$(curl -s "https://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$LLVM_REL_VER/" | sed 's/<[^>]*>//g' | grep "rc\|final" | head -1 | sed 's/[[:blank:]]\|\///g')
-LLVM_REL_STATUS_ARG="--build-arg=LLVM_REL_STATUS=${LLVM_REL_STATUS}"
-printf "using LLVM_REL_STATUS_ARG = %s\n" "$LLVM_REL_STATUS_ARG"
 
-docker build "$LLVM_REL_VER_ARG" "$LLVM_REL_STATUS_ARG" --build-arg="JW_DOCKER_BUILD_DIR=$JW_DOCKER_BUILD_DIR" -t "jackwasey/$JW_DOCKER_BUILD_DIR" -f "$JW_DOCKER_BUILD_DIR/Dockerfile" .
+if [ -n "$LLVM_REL_STR" ]; then
+    LLVM_REL_VER=$(curl -s 'https://llvm.org/svn/llvm-project/llvm/tags/' | sed 's/<[^>]*>//g' | grep "$LLVM_REL_STR" | sed 's/^[[:space:]]*RELEASE_//g' | sed 's/\/$//' | tail -1)
+    printf "using LLVM_REL_VER=%s\n" "$LLVM_REL_VER"
+    LLVM_REL_VER_ARG=--build-arg=LLVM_REL_VER=$LLVM_REL_VER
+    printf "using LLVM_REL_VER_ARG=%s\n" "$LLVM_REL_VER_ARG"
+
+    LLVM_REL_STATUS=$(curl -s "https://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$LLVM_REL_VER/" | sed 's/<[^>]*>//g' | grep "rc\|final" | head -1 | sed 's/[[:blank:]]\|\///g')
+    printf "using LLVM_REL_STATUS=%s\n" "$LLVM_REL_STATUS"
+    LLVM_REL_STATUS_ARG=--build-arg=LLVM_REL_STATUS=$LLVM_REL_STATUS
+    printf "using LLVM_REL_STATUS_ARG=%s\n" "$LLVM_REL_STATUS_ARG"
+fi
+docker build \
+             $LLVM_REL_VER_ARG \
+             $LLVM_REL_STATUS_ARG \
+             --build-arg="JW_DOCKER_BUILD_DIR=$JW_DOCKER_BUILD_DIR" \
+             -t "jackwasey/$JW_DOCKER_BUILD_DIR" \
+             -f "$JW_DOCKER_BUILD_DIR/Dockerfile" \
+             . 
+
