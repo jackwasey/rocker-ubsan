3b7f63ae187724668d66ec31d16b5a0ee7905044
Jack Wasey <jack@jackwasey.com>
2016-08-28 20:06:07 -0400

    get-r refined

--- get-r.sh
+++ get-r.sh
@@ -2,12 +2,35 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ -v JW_R_SVN ]]
-then
-    curl https://cran.rstudio.com/src/base-prerelease/R-latest.tar.gz | tar xz
-    mv R-* R-devel
-else
-## Check out R-devel, but it doesn't always compile, but the tar balls
-    svn co https://svn.r-project.org/R/trunk R-devel
+#########################################
+# Download a version of R in 3.x series #
+#########################################
+
+VER=${1:-svn}
+
+echo "Version $VER of R requested for download"
+
+if [ -d "R-devel" ]; then
+    echo "R-devel directory already exists"
+    exit 1
 fi
+
+# example URL: https://cloud.r-project.org/src/base/R-3/R-3.0.0.tar.gz
+URL_BASE="https://cloud.r-project.org/src/base/R-3/"
+
+if [[ "$VER" = "svn" ]]; then
+    ## Check out R-devel, but it doesn't always compile, whereas the tar balls probably do
+    svn co https://svn.r-project.org/R/trunk R-devel
+elif [[ "$VER" = "rel" ]]; then
+    URL="https://cran.rstudio.com/src/base/R-latest.tar.gz"
+elif [[ "$VER" = "pre" ]]; then
+    URL="https://cran.rstudio.com/src/base-prerelease/R-latest.tar.gz"
+elif [[ $VER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    URL="${URL_BASE}/R-${VER}.tar.gz"
+else
+    echo "Version ${VER} not recognized"
+    exit 1
+fi
+curl "$URL" | tar xz
+mv R-* R-devel
 
