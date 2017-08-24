c7376d37988cfe04c74da3e16986f3ada984bca9
Jack Wasey <jack@jackwasey.com>
2017-08-24 08:06:31 -0400

    keeping up with versions

---
+++ bin/install.r
@@ -0,0 +1,3 @@
+#!/usr/local/bin/RDscript
+
+install.packages(c('codetools', 'testthat', 'Rcpp', 'RColorBrewer', 'dichromat', 'munsell', 'checkmate', 'scales', 'proto', 'caTools', 'evaluate', 'knitr', 'xtable', 'rmarkdown', 'fastmatch', 'RODBC', 'roxygen2', 'xml2'), INSTALL_opts = '--no-byte-compile')
